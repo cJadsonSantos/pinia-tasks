3ca17dcb19f7ab365f598fbdd1bbbb3781bd12ac
Jadson <jadsonsantos@aformulabr.com.br>
2023-06-20 16:15:27 -0300

    'change'

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,24 @@
         <!-- heading -->
         <header>
             <img src="./assets/pinia-logo.svg" alt="pinia logo">
-            <h1>{{ taskStore.name }}</h1>
+            <h1>Pinia Tasks</h1>
         </header>
+
+        <!-- task list-->
+        <div class="task-list">
+            <div v-for="task in taskStore.tasks" :key="task.id">
+                <TaskDetails :task="task"/>
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
-
+import TaskDetails from "@/components/TaskDetails.vue";
 import {useTaskStore} from "@/stores/TaskStore";
 
 export default {
+    components: {TaskDetails},
     setup() {
         const taskStore = useTaskStore()
         return {taskStore}
