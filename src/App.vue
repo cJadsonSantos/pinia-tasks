93508aafe219887b6657b284c9c747c9a0980469
cjadsonsantos <cjadsonsantos@hotmail.com>
2023-06-24 19:25:42 -0300

    'change'

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,55 @@
 <template>
-    <main>
-        <!-- heading -->
-        <header>
-            <img src="./assets/pinia-logo.svg" alt="pinia logo">
-            <h1>Pinia Tasks</h1>
-        </header>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm/>
+    </div>
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
 
-        <!-- filter -->
-        <nav class="filter">
-            <button @click="filter = 'all'">All tasks</button>
-            <button @click="filter = 'favs'">Fav tasks</button>
-        </nav>
-        <!-- task list-->
-        <div class="task-list" v-if="filter === 'all'">
-            <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
-            <div v-for="task in taskStore.tasks" :key="task.id">
-                <TaskDetails :task="task"/>
-            </div>
-        </div>
-        <div class="task-list" v-if="filter === 'favs'">
-            <p>Your have {{ taskStore.favCount }} favs left to do</p>
+    <!-- loading -->
+    <div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+    <!-- task list-->
+    <div class="task-list" v-if="filter === 'all'">
+      <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>Your have {{ taskStore.favCount }} favs left to do</p>
 
-            <div v-for="task in taskStore.favs" :key="task.id">
-                <TaskDetails :task="task"/>
-            </div>
-        </div>
-    </main>
+      <div v-for="task in taskStore.favs" :key="task.id">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 import TaskDetails from "@/components/TaskDetails.vue";
 import {useTaskStore} from "@/stores/TaskStore";
 import {ref} from "vue";
+import TaskForm from "@/components/TaskForm.vue";
 
 export default {
-    components: {TaskDetails},
-    setup() {
-        const taskStore = useTaskStore()
+  components: {TaskForm, TaskDetails},
+  setup() {
+    const taskStore = useTaskStore()
 
-        const filter = ref('all')
-        return {taskStore, filter}
-    }
+    // fetch tasks
+    taskStore.getTasks()
+
+    const filter = ref('all')
+    return {taskStore, filter}
+  }
 }
 </script>
